3ed1441ae18757547fd3ac34fa8db02e7ae78541
Derrick Alcius <derrickalcius@gmail.com>
2023-05-15 21:52:19 -0400

    changed files, insomnia is working

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const { connect, connection } = require('mongoose');
+
+//connects to database with a link
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia';
+
+connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
